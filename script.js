c6d1285e2e061bd14092881caa5e02bb5b504a71
ZarmDev <80705328+ZarmDev@users.noreply.github.com>
2024-10-13 11:11:56 -0400

    Updated quill js + added strikethrough option. Also cleaned up code a bit

--- script.js
+++ script.js
@@ -13,16 +13,17 @@ let fonts = Quill.import("attributors/style/font");
 fonts.whitelist = fontFamilyArr;
 Quill.register(fonts, true);
 
+const colorOptions = ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466', 'custom-color'];
 
 // thanks to https://stackoverflow.com/questions/42068335/quill-js-color-textbox
 const modulesOptions = {
-    toolbar: [
+    container: [
         [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
         [{ 'font': fontFamilyArr }],
         [{ 'size': fontSizeArr }],
         [{ 'align': [] }],
-        ['bold', 'italic', 'underline'],
-        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'color': ['#000000', '#e60000', '#ff9900', '#ffff00', '#008a00', '#0066cc', '#9933ff', '#ffffff', '#facccc', '#ffebcc', '#ffffcc', '#cce8cc', '#cce0f5', '#ebd6ff', '#bbbbbb', '#f06666', '#ffc266', '#ffff66', '#66b966', '#66a3e0', '#c285ff', '#888888', '#a10000', '#b26b00', '#b2b200', '#006100', '#0047b2', '#6b24b2', '#444444', '#5c0000', '#663d00', '#666600', '#003700', '#002966', '#3d1466', 'custom-color'] }, { 'background': [] }, 'link'],
+        ['bold', 'italic', 'underline', 'strike'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'color': colorOptions}, { 'background': colorOptions }, 'link'],
     ],
     // pretty cool custom key bindings
     // keyboard: {
@@ -35,19 +36,27 @@ const modulesOptions = {
     //         }
     //     }
     // },
+    handlers: {
+        color: function (value) {
+            if (value == 'custom-color') value = window.prompt('Enter Hex Color Code');
+            this.quill.format('color', value);
+        }
+    }
 };
 
-const formatsOptions = [
-    'header', 'font', 'size',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'indent', 'align',
-    'link', 'image', 'background', 'color'
-];
+// const formatsOptions = [
+//     'header', 'font', 'size',
+//     'bold', 'italic', 'underline', 'strike', 'blockquote',
+//     'list', 'indent', 'align',
+//     'link', 'image', 'background', 'color'
+// ];
 
 document.addEventListener("DOMContentLoaded", function () {
     var quill = new Quill('#content', {
-        formats: formatsOptions,
-        modules: modulesOptions,
+        // formats: formatsOptions,
+        modules: {
+           toolbar: modulesOptions,
+        },
         theme: 'snow'
     })
 });
